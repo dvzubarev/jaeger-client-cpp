849f28e8174e2d5f7010fc03d07fa8a2d01cc623
Deepika Upadhyay <deepikaupadhyay01@gmail.com>
2019-08-09 17:11:25 +0530

    adds cmake files: json parser and yaml-cpp

--- examples/App.cpp
+++ examples/App.cpp
@@ -1,3 +1,4 @@
+#include <boost/type_index.hpp>
 #include <iostream>
 
 #include <yaml-cpp/yaml.h>
@@ -6,14 +7,21 @@
 
 namespace {
 
-void setUpTracer(const char* configFilePath)
+std::shared_ptr<opentracing::Tracer> setUpTracer(const char* configFilePath)
 {
     auto configYAML = YAML::LoadFile(configFilePath);
     auto config = jaegertracing::Config::parse(configYAML);
+    std::cout << boost::typeindex::type_id<decltype(config)>().pretty_name()
+              << std::endl;
+    std::cout << config.disabled();
+    // std::cout << config.sampler();
     auto tracer = jaegertracing::Tracer::make(
         "example-service", config, jaegertracing::logging::consoleLogger());
     opentracing::Tracer::InitGlobal(
         std::static_pointer_cast<opentracing::Tracer>(tracer));
+    std::cout << boost::typeindex::type_id<decltype(tracer)>().pretty_name()
+              << std::endl;
+    return tracer;
 }
 
 void tracedSubroutine(const std::unique_ptr<opentracing::Span>& parentSpan)
@@ -22,13 +30,42 @@ void tracedSubroutine(const std::unique_ptr<opentracing::Span>& parentSpan)
         "tracedSubroutine", { opentracing::ChildOf(&parentSpan->context()) });
 }
 
-void tracedFunction()
+const std::unique_ptr<opentracing::Span> tracedFunction()
 {
     auto span = opentracing::Tracer::Global()->StartSpan("tracedFunction");
     tracedSubroutine(span);
+    span->Finish();
+    return span;
 }
 
+static std::string inject(const char* name)
+{
+    std::stringstream ss;
+    auto jspan = opentracing::Tracer::Global()->StartSpan("test");
+    if (!jspan) {
+        auto span = opentracing::Tracer::Global()->StartSpan(name);
+    }
+    auto err = opentracing::Tracer::Global()->Inject(jspan->context(), ss);
+    return ss.str();
+}
+static void extract(const std::unique_ptr<opentracing::Span> span,
+                    const char* name,
+                    std::string t_meta)
+{
+    std::stringstream ss(t_meta);
+    //    if(!tracer){
+    //    }
+    // setUpTracer("Extract-service");
+    auto span_context_maybe = opentracing::Tracer::Global()->Extract(ss);
+    assert(span_context_maybe);
+
+    // Propogation span
+    auto _span = opentracing::Tracer::Global()->StartSpan(
+        "propagationSpan", { opentracing::ChildOf(span_context_maybe->get()) });
+
+    auto span1 = std::move(_span);
 }  // anonymous namespace
+}  // namespace
 
 int main(int argc, char* argv[])
 {
@@ -36,10 +73,14 @@ int main(int argc, char* argv[])
         std::cerr << "usage: " << argv[0] << " <config-yaml-path>\n";
         return 1;
     }
-    setUpTracer(argv[1]);
-    tracedFunction();
+    auto tracer = setUpTracer(argv[1]);
+
+    auto kspan = opentracing::Tracer::Global()->StartSpan("test");
+    std::string t_meta = inject("inject");
+    std::cout << t_meta;
     // Not stricly necessary to close tracer, but might flush any buffered
     // spans. See more details in opentracing::Tracer::Close() documentation.
     opentracing::Tracer::Global()->Close();
     return 0;
 }
+
